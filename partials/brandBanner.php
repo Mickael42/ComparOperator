6c21a4f280abfd27a8973c45e11670159b9bddac
Mickael42 <48717155+Mickael42@users.noreply.github.com>
2019-06-12 17:56:40 +0200

    VF

---
+++ partials/brandBanner.php
@@ -0,0 +1,19 @@
+<div class="container" id="containerBrandBanner">
+    <div class="row justify-content-center">
+        <div class="col-md colBrandBanner">
+        <img class="iconsBrandBanner" src="..\assets\img\icons\calendar.png" alt="">
+            <h4>Planifier</h4>
+            <p>Confiez-nous vos rêves d'évasion : en famille ou entre amis, nous trouverons la formule qui comblera vos attentes.</p>
+        </div>
+        <div class="col-md colBrandBanner">
+        <img class="iconsBrandBanner" src="..\assets\img\icons\luggage.png" alt="">
+            <h4>Organiser</h4>
+            <p>Bénéficiez de l'expertise de nos spécialistes de chaque destination, ils vous accompagnent dans la réalisation de votre voyage.</p>
+        </div>
+        <div  class="col-md colBrandBanner">
+            <img class="iconsBrandBanner" src="..\assets\img\icons\around.png" alt="">
+            <h4>Voyager</h4>
+            <p>Nous nous chargeons d'assurer votre sécurité et de veiller à votre pleine sérénité tout au long de votre voyage.</p>
+        </div>
+    </div>
+</div>
---
+++ partials/footer.php
@@ -0,0 +1,26 @@
+
+<!-- Footer -->
+<footer>
+<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 ">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><img src="..\assets\img\icons\facebook-logo-button.png" class="fa fa-facebook"></img></li>
+						<li class="list-inline-item"><img src="..\assets\img\icons\linkedin-logo.png" class="fa fa-linkedin"></img></li>
+						<li class="list-inline-item"><img src="..\assets\img\icons\twitter-logo.png" class="fa fa-twitter"></img></li>
+						<li class="list-inline-item"><img src="..\assets\img\icons\google-logo.png"  class="fa fa-google-plus"></img></li>
+					</ul>
+				</div>
+				</hr>
+			</div>	
+			<div class="row">
+				<div class="col-sm-12 text-center text-white">
+					<p><u><a href="#body">ComparOperator.com</a></u> est un site développé par la C&M Web Agency.</p>
+					<p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">C&M Web Agency</a></p>
+				</div>
+				</hr>
+			</div>	
+		</div>
+	</section>
+</footer>
